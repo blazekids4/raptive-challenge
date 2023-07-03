aff6ec1baa62234bf46435b87dfdd8e5ee108769
Justin Lyons <45666337+lilsurepbc@users.noreply.github.com>
2023-07-02 20:46:36 -0500

    fix build error

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,14 +21,12 @@ export default function Home() {
   return (
     <>
       <Card className="mx-3 sm:p-3 overflow-auto">
-        <div className="flex flex-col items-center m-4 py-2">
+        <div className="flex flex-col items-left   m-4 py-2">
           <br />
           <h1 className="text-4xl text-rap4">Hello Raptive</h1>
-          <p className="text-xl text-rap4">Let&apos;s Party</p>
+          <p className="text-xl ml-3 text-rap4">Let&apos;s Party</p>
         </div>
-        <Card className="items-left">
-          <UploadForm />
-        </Card>
+
         <Card className="m-4 items-left">
           <TabGroup>
             <TabList className="mt-8 text-rap4 font-semibold">
